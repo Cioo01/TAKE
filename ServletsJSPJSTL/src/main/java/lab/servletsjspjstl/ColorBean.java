da9081ea23f33bc270d26cd206cec0e7310ea121
we wer <jakub.ciuleba@gmail.com>
2024-03-14 14:25:16 +0100

    lab1

---
+++ ServletsJSPJSTL/src/main/java/lab/servletsjspjstl/ColorBean.java
@@ -0,0 +1,30 @@
+package lab.servletsjspjstl;
+
+public class ColorBean {
+    private String foregroundColor;
+    private String backgroundColor;
+    private String borderChoice;
+
+    public String getForegroundColor() {
+        return foregroundColor;
+    }
+
+    public void setForegroundColor(String foregroundColor) {
+        this.foregroundColor = foregroundColor;
+    }
+
+    public String getBackgroundColor() {
+        return backgroundColor;
+    }
+
+    public void setBackgroundColor(String backgroundColor) {
+        this.backgroundColor = backgroundColor;
+    }
+    public String getBorderChoice() {
+        return borderChoice;
+    }
+
+    public void setBorderChoice(String borderChoice) {
+        this.borderChoice = borderChoice;
+    }
+}
